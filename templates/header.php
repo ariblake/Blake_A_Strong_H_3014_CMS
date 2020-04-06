58513fc3cf12a8cd3bd15a1b67244fe58dc51070
Ariana Blake <a_blake69703@fanshaweonline.ca>
2020-04-05 22:01:37 -0400

    added some styling

--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,15 @@
 <header>
-    <h2>Sportchek</h2>
+    <h1>Sportchek</h1>
     <nav class="filterNav">
-        <li><a href="index.php?filter=Men">Men</a></li>
-        <li><a href="index.php?filter=Women">Women</a></li>
-        <li><a href="index.php?filter=Kids">Kids</a></li>
-        <li><a href="index.php?filter=Footwear">Footwear</a></li>
-        <li><a href="index.php?filter=Gear">Gear</a></li>
-        <li><a href="index.php?filter=Electronics">Electronics</a></li>
-        <li><a href="index.php?filter=Jerseys">Jerseys & Fan Wear</a></li>
-        <li><a href="index.php">All</a></li>
+        <ul>
+            <li><a href="index.php?filter=Men">Men</a></li>
+            <li><a href="index.php?filter=Women">Women</a></li>
+            <li><a href="index.php?filter=Kids">Kids</a></li>
+            <li><a href="index.php?filter=Footwear">Footwear</a></li>
+            <li><a href="index.php?filter=Gear">Gear</a></li>
+            <li><a href="index.php?filter=Electronics">Electronics</a></li>
+            <li><a href="index.php?filter=Jerseys">Jerseys & Fan Wear</a></li>
+            <li><a href="index.php">All</a></li>
+        </ul>
     </nav>
 </header>
